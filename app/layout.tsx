b6ed35ca9e8f3b09b673ac1c9dabbfb4afc7e3cd
mciccale <marcociccalebaztan@gmail.com>
2024-01-10 13:21:06 +0100

    feature: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
@@ -44,6 +45,7 @@ export default function RootLayout({
       <body className={`${montserrat.className} mt-24 dark:bg-gray-900`}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
